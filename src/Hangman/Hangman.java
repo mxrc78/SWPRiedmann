20dba0401aac7b7df9127a4dd3ddf6392de5183b
mxrc78 <madlenermarc@gmail.com>
2022-04-29 12:17:15 +0200

    update

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,22 @@
+package Hangman;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Hangman {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String Raten = scanner.next();
+
+        ArrayList<String> Wörter = new ArrayList<String>();
+        Wörter.add("Eisenbahn");
+        Wörter.add("Auto");
+        Wörter.add("Flugzeug");
+        Wörter.add("Frankreich");
+
+
+
+    }
+}
